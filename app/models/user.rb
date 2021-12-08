e49189e86fdb06596e08c3218ac065fbc3e29433
shengchao <ishengchao@outlook.com>
2021-12-08 16:05:10 +0800

    model user添加验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }  # self.email = email.downcase
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true  # case_sensitive: false，Rails 会自动指定 :uniqueness 的值为 true。
+
+
 end
