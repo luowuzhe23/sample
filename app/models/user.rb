54cd138e2bedda4d67650985e3b2c20267a9cfda
shengchao <ishengchao@outlook.com>
2021-12-08 16:31:05 +0800

    model user添加密码

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: true  # case_sensitive: false，Rails 会自动指定 :uniqueness 的值为 true。
+  validates :password, presence: true, length: { minimum: 6 }  # 密码最低6位
   has_secure_password    # 对应的模型中有个名为 password_digest 的属性
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "zhangsan", email: "zhangsan@gamil.com")
+    @user = User.new(name: "zhangsan", email: "zhangsan@gamil.com",
+                     password: "123456", password_confirmation: "123456")
   end
 
   test "should be vaild" do
@@ -57,6 +58,16 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 
